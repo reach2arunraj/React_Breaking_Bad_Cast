65f951654aa845767f38397bbaae1f45c4104238
reach2arunraj <60217918+reach2arunraj@users.noreply.github.com>
2020-07-04 18:11:40 +0530

    CharacterGrid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Hearder from "./components/Display/Header"
+import CharacterGrid from "./components/Display/characters/CharacterGrid"
+
 import './App.css';
 
-function App() {
+
+const  App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    const fetchItem = async () => {
+      const result = await fetch("https://www.breakingbadapi.com/api/characters").then((data) => data.json())
+      setItems(result)
+      setIsLoading(false)
+    }
+    fetchItem()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Hearder />
+      <CharacterGrid isLoading={isLoading} items={items}/>
     </div>
   );
 }
---
+++ src/components/Display/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+import logo from "../../img/logo.png"
+
+const Hearder = () => {
+    return (
+        <header className="center">
+            <img src={logo} alt="" />
+        </header>
+    )
+}
+
+export default Hearder
